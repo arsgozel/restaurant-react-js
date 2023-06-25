124948b9dd9e621cc873ab58d12e792d708cb564
Gozel <30gozel30gozel@gmail.com>
2023-06-25 16:34:06 +0500

    1.3.1

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -12,8 +12,11 @@ const AboutUs = () => (
     <div className="app__aboutus-content flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
+
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+
         <p className="p__opensans">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
+
         <button type="button" className="custom__button">Know More</button>
       </div>
 
@@ -23,8 +26,11 @@ const AboutUs = () => (
 
       <div className="app__aboutus-content_history">
         <h1 className="headtext__cormorant">Our History</h1>
+
         <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+
         <p className="p__opensans">Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+        
         <button type="button" className="custom__button">Know More</button>
       </div>
     </div>
